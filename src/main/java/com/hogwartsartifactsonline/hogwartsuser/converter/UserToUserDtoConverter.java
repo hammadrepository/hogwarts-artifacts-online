b381ec49cab4869c926e8c51853def0624ac30b0
hammadrashid <hammadrashid98@gmail.com>
2023-10-09 00:40:12 +0300

    User crud finished! Close #4

---
+++ src/main/java/com/hogwartsartifactsonline/hogwartsuser/converter/UserToUserDtoConverter.java
@@ -0,0 +1,21 @@
+package com.hogwartsartifactsonline.hogwartsuser.converter;
+
+import com.hogwartsartifactsonline.hogwartsuser.HogwartsUser;
+import com.hogwartsartifactsonline.hogwartsuser.dto.UserDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserToUserDtoConverter implements Converter<HogwartsUser, UserDto> {
+
+    @Override
+    public UserDto convert(HogwartsUser source) {
+        // We are not setting password in DTO.
+        final UserDto userDto = new UserDto(source.getId(),
+                                            source.getUsername(),
+                                            source.isEnabled(),
+                                            source.getRoles());
+        return userDto;
+    }
+
+}
